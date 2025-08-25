0518d229129ccef6fc8ca4ce4c125bd83d9e24e7
nikhil-partap <nikhilpartapsingh88@gmail.com>
2025-08-25 11:06:31 +0530

    Add if-else statements implementation for various checks and classifications

---
+++ python/4day/if_else.py
@@ -0,0 +1,64 @@
+# writing if else statements in one line 
+# like 
+a = 2
+b = 4 
+print("kjdasfh") if a > b else print("nkldskgjzd") 
+
+"""
+write a pg to checkk if a no is even or odd
+write a pg to checkk if input is a vovel 
+write a pg to checkk if a no is multiple of 5 
+write a pg to checkk if a no divisible by seven or not 
+write a pg to classify  PERSONs age gp child < 13 tenage 13 - 19 adult 20 - 59 senior 60+
+write a pg to asign a grade based on the marks 90+ ==a   75 - 90 b  50- 75 c 0-50 f(fail)
+"""
+
+
+
+
+x = int(input("x: "))
+if x%2 == 0 :
+    print("the no is even")
+else :
+    print("no is not even")
+
+
+y = input("enter a alphabet")
+if (y.lower() == "a" or y.lower()== "e" or y.lower()=="i" or y.lower()=="o" or y.lower()=="u" ):
+    print(f"{y}  is a vovel")
+else :
+    print(f"{y} is not a vovel")
+    
+y = int(input("input a no to check if no is multiple by 5: "))
+if y%5 == 0:
+    print("no is multiple by 5")
+else:
+    print("no is not multiple by 5")
+
+z = int(input("enter a no to check if it is divisible by 7"))
+if y%7 == 0:
+    print("no is multiple by 7")
+else:
+    print("no is not multiple by 7")
+
+
+a = int(input("Enter your age to find your age gp"))
+if a <= 13:
+    print("age gp child")
+if a > 13 and a <=19:
+    print("age gp Teenage")
+if a > 20 and a <=59:
+    print("age gp Adult")
+if a >= 60:
+    print("age gp Senior")
+
+
+a = int(input("Enter your Marks to get your grade"))
+if a >= 90:
+    print("grade - A")
+if a >= 75 and a <90:
+    print("grade - B")
+if a >=50 and a < 75:
+    print("grade - C")
+if a < 50:
+    print("grade - F")
