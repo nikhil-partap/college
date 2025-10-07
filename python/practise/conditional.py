0842efadf9004ea0abdd6863ec4864fd09e5c7f5
nikhil-partap <nikhilpartapsingh88@gmail.com>
2025-10-07 22:41:34 +0530

    Add Python scripts for cinema ticket pricing and e-commerce discount calculations, along with various pattern printing examples

---
+++ python/practise/conditional.py
@@ -0,0 +1,28 @@
+# # A cinema hall charges ₹200 for adults and ₹150 for children. If the person’s age is below 12,
+# # they are considered a child; otherwise, they are an adult.
+# # Write a program to calculate the ticket price based on age.
+
+
+# age = int(input("Enter Your age: "))
+# noOfTickets = int(input("how many tickets do you want : "))
+# price = 0
+# if age < 12 :
+#     price = 150 
+# else :
+#     price = 200
+
+# ticket_price = price*noOfTickets
+
+# print(ticket_price)
+
+
+
+
+# An e-commerce site gives discounts:
+#  If bill amount ≥ ₹5000 → 20% discount
+#  If bill amount ≥ ₹3000 → 15% discount
+#  If bill amount ≥ ₹1000 → 10% discount
+#  Otherwise → No discount
+# Write a program to calculate the final price after discount.
+
+
---
+++ python/practise/patterns.py
@@ -0,0 +1,98 @@
+#     2. Print a right triangle:
+# *
+# * *
+# * * *
+# * * * *
+
+
+# for i in range(5):
+#     for j in range(i+1):
+#         print("*", end="")
+#     print()
+
+
+#     3. Print an inverted right triangle:
+# * * * *
+# * * *
+# * *
+# *
+
+
+# n= 5
+# for i in range(n):
+#     for j in range(n-i):
+#         print("*", end="")
+#     print()
+
+
+
+#     4. Print a pyramid:
+#    *
+#   * *
+#  * * *
+# * * * *
+
+# n= 5
+# for i in range(1,n+1):
+#     front = " "*(n-i)
+#     middle = " ".join("*"*(i))
+#     print(front + middle )
+
+
+
+#     5. Print an inverted pyramid:
+# * * * *
+#  * * *
+#   * *
+#    *
+
+# n  = 4
+# for i in range(1,n+1):
+#     front = " "*(i-1)
+#     middle = " ".join("*"*((n-i)+1))
+#     print(front+middle)
+
+
+
+
+#     6. Print numbers in increasing triangle:
+# 1
+# 1 2
+# 1 2 3
+# 1 2 3 4
+
+# for i in range(5):
+#     for j in range(1,i+2):
+#         print(j,end="")
+#     print()
+
+
+
+#     7. Print numbers in a square:
+# 1 2 3 4
+# 1 2 3 4
+# 1 2 3 4
+# 1 2 3 4
+
+# n= 4
+# for i in range(1,n):
+#     for j in range(1,n+1):
+#         print(j, end="")
+#     print()
+
+
+
+#     8. Print repeated row numbers:
+# 1
+# 2 2
+# 3 3 3
+# 4 4 4 4
+
+# n = 5
+# for i in range(1,n+1):
+#     for j in range(i):
+#         print(i,end="")
+#     print()
+
+
+
