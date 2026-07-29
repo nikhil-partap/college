702b6c7e28496bcea02d7e2d713defa9c87a4dbb
nikhil-partap <your-email@example.com>
2026-07-29 15:25:02 +0530

    aaaaaaaaaaaaa

---
+++ sem3/java_self/bro/Main.java
@@ -0,0 +1,24 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Student s1 = new Student("Nikhil", 101, 91.5);
+        Student s2 = new Student("Rahul", 102, 67.0);
+        Student s3 = new Student("Aman", 103, 35.5);
+
+        System.out.println("Student 1");
+        s1.display();
+        System.out.println("\nPassed : " + s1.isPassed());
+
+        System.out.println("\n-----------------------");
+
+        System.out.println("Student 2");
+        s2.display();
+        System.out.println("\nPassed : " + s2.isPassed());
+
+        System.out.println("\n-----------------------");
+
+        System.out.println("Student 3");
+        s3.display();
+        System.out.println("\nPassed : " + s3.isPassed());
+    }
+}
---
+++ sem3/java_self/bro/Student.java
@@ -0,0 +1,27 @@
+public class Student {
+    String name;
+    int rollNo;
+    double marks;
+
+    Student(String name, int rollNo, double marks){
+        this.name = name;
+        this.rollNo = rollNo;
+        this.marks = marks;
+    }
+
+    // Name : Nikhil
+    // Roll No : 101
+    // Marks : 91.5
+    void display(){
+        System.out.print("Name : " +name + "\n" + "Roll No : " + rollNo + "\n" + "Marks : " + marks);
+    }
+
+    boolean isPassed(){
+        if(this.marks < 40){
+            return false;
+        } else {
+            return true;
+        }
+    }
+    
+}
